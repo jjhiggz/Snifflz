520b530a778c4a6547778c88a493f69b58881c5d
jjhiggz <jonathan.higger@gmail.com>
2020-08-22 11:40:16 -0600

    first commitg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import HomepageLayout from './HomepageLayout';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HomepageLayout />
   </React.StrictMode>,
   document.getElementById('root')
 );
