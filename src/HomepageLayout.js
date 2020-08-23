ecd7c92be41ce3d08524a45e6af00a53192c71eb
Desiree <descxcrs@gmail.com>
2020-08-23 11:25:50 -0400

    pair programming update

--- src/HomepageLayout.js
+++ src/HomepageLayout.js
@@ -1,169 +1,140 @@
-import "semantic-ui-css/semantic.min.css";
-import { createMedia } from "@artsy/fresnel";
-import PropTypes from "prop-types";
-import React, { Component } from "react";
+import 'semantic-ui-css/semantic.min.css'
+import PropTypes from 'prop-types'
+import React, { useState, useEffect } from 'react'
 import {
-  Button,
   Container,
-  Grid,
   Header,
-  Icon,
-  Image,
-  Menu,
   Segment,
-  Sidebar,
-  Visibility,
-} from "semantic-ui-react";
+} from 'semantic-ui-react'
+import { ZipForm } from './Components/ZipForm'
+import Results from './Components/Results'
 
-const { MediaContextProvider, Media } = createMedia({
-  breakpoints: {
-    mobile: 0,
-    tablet: 768,
-    computer: 1024,
-  },
-});
+const BREEZE_O_METER_API_KEY = 'aa2ca4cd977844dba365b5d0434ec9e6'
+const GOOGLE_MAPS_API_KEY = 'AIzaSyD3At_jejDYID03nsPV38JNuj_JClsSf58'
+const ZIP_ENDPOINT = 'https://maps.googleapis.com/maps/api/geocode/json?key='+ GOOGLE_MAPS_API_KEY + '&components=postal_code:'
+
+const getBreezeURL = (lat, lng, key) => {
+  return `https://api.breezometer.com/pollen/v2/forecast/daily?lat=${lat}&lon=${lng}&key=${key}&days=3`
+}
+
 
 /* Heads up!
  * HomepageHeading uses inline styling, however it's not the best practice. Use CSS or styled
  * components for such things.
  */
-const HomepageHeading = ({ mobile }) => (
+export const HomepageHeading = ({ setZip }) => (
   <Container text>
     <Header
-      as="h1"
-      content="SnifflZ"
+      as='h1'
+      content='SnifflZ'
       inverted
       style={{
-        fontSize: mobile ? "2em" : "4em",
-        fontWeight: "normal",
+        fontSize:'4em',
+        fontWeight: 'normal',
         marginBottom: 0,
-        marginTop: mobile ? "1.5em" : "3em",
+        marginTop:'3em',
       }}
     />
     <Header
-      as="h2"
-      content="This season quit sneezin"
+      as='h2'
+      content='Tis the season to quit sneezin'
       inverted
       style={{
-        fontSize: mobile ? "1.5em" : "1.7em",
-        fontWeight: "normal",
-        marginTop: mobile ? "0.5em" : "1.5em",
+        fontSize:  '1.7em',
+        fontWeight: 'normal',
+        marginTop: '1.5em',
       }}
     />
-    <Button primary size="huge">
-      Get Started
-      <Icon name="right arrow" />
-    </Button>
+    <ZipForm
+      setZip={setZip}
+    />
   </Container>
-);
-
-HomepageHeading.propTypes = {
-  mobile: PropTypes.bool,
-};
+)
 
 /* Heads up!
  * Neither Semantic UI nor Semantic UI React offer a responsive navbar, however, it can be implemented easily.
  * It can be more complicated, but you can create really flexible markup.
  */
-class DesktopContainer extends Component {
-  state = {};
-
-  hideFixedMenu = () => this.setState({ fixed: false });
-  showFixedMenu = () => this.setState({ fixed: true });
-
-  render() {
-    const { children } = this.props;
-    const { fixed } = this.state;
+const DesktopContainer = ({ setZip }) => {
 
     return (
-      <Media greaterThan="mobile">
-        <Visibility
-          once={false}
-          onBottomPassed={this.showFixedMenu}
-          onBottomPassedReverse={this.hideFixedMenu}
-        >
           <Segment
             inverted
-            textAlign="center"
-            style={{
-              minHeight: 700,
-              padding: "1em 0em",
-              opacity: 0.9,
-              backgroundImage: `url(${"https://images.unsplash.com/photo-1574193087288-ab6c6ef2933c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2800&q=80"})`,
-              backgroundSize: "cover",
-            }}
+            textAlign='center'
+            style={{  padding: '1em 0em' }}
             vertical
+            
           >
-            <Menu
-              fixed={fixed ? "top" : null}
-              inverted={!fixed}
-              pointing={!fixed}
-              secondary={!fixed}
-              size="large"
-            >
-              <Container>
-                <Menu.Item as="a" active>
-                  Home
-                </Menu.Item>
-                <Menu.Item as="a">Work</Menu.Item>
-                <Menu.Item as="a">Company</Menu.Item>
-                <Menu.Item as="a">Careers</Menu.Item>
-                <Menu.Item position="right">
-                  <Button as="a" inverted={!fixed}>
-                    Log in
-                  </Button>
-                  <Button
-                    as="a"
-                    inverted={!fixed}
-                    primary={fixed}
-                    style={{ marginLeft: "0.5em" }}
-                  >
-                    Sign Up
-                  </Button>
-                </Menu.Item>
-              </Container>
-            </Menu>
-            <HomepageHeading />
+            <HomepageHeading
+              setZip={setZip}
+              style={{
+                height: '100%'
+              }}
+            />
           </Segment>
-        </Visibility>
-
-        {children}
-      </Media>
-    );
-  }
+    )
 }
 
-const HomepageLayout = () => (
-  <DesktopContainer>
-    <Segment style={{ padding: "8em 0em" }} vertical>
-      <Grid container stackable verticalAlign="middle">
-        <Grid.Row>
-          <Grid.Column width={8}>
-            <Header as="h3" style={{ fontSize: "2em" }}>
-              We Help Companies and Companions
-            </Header>
-            <p style={{ fontSize: "1.33em" }}>
-              We can give your company superpowers to do things that they never
-              thought possible. Let us delight your customers and empower your
-              needs... through pure data analytics.
-            </p>
-            <Header as="h3" style={{ fontSize: "2em" }}>
-              We Make Bananas That Can Dance
-            </Header>
-            <p style={{ fontSize: "1.33em" }}>
-              Yes that's right, you thought it was the stuff of dreams, but even
-              bananas can be bioengineered.
-            </p>
-          </Grid.Column>
-        </Grid.Row>
-        <Grid.Row>
-          <Grid.Column textAlign="center">
-            <Button size="huge">Check Them Out</Button>
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
-    </Segment>
-  </DesktopContainer>
-);
+DesktopContainer.propTypes = {
+  children: PropTypes.node,
+}
 
-export default HomepageLayout;
+const HomepageLayout = () => {
+  const [ zip , setZip ] = useState('')
+  const [ coord, setCoord ] = useState({
+    isEmpty: true,
+    lat:0,
+    lng:0,
+  })
+  const [ pollen, setPollen ] = useState({})
+  const [ fireScore, setFireScore ] = useState( null )
+  const [ airscore, airScore ] = useState( null )
+  
+
+  useEffect(()=>{
+    // https://maps.googleapis.com/maps/api/geocode/outputFormat?parameters
+    const MAPS_URL = ZIP_ENDPOINT + zip
+    
+
+    fetch(MAPS_URL)
+      .then(response => response.json())
+      .then(returnObject => {
+        let results = returnObject.results
+        if( results.length > 0 && results[0].hasOwnProperty('geometry')){
+          const location = returnObject.results[0].geometry.location
+          setCoord({
+            lat:location.lat,
+            lng:location.lng,
+            isEmpty:false,
+          })
+          fetchPollen( location.lat, location.lng )
+        }
+      })
+    
+    
+
+  }, [zip] )
+
+  const fetchPollen = (lat, lng) => {
+    const URL = getBreezeURL(lat, lng, BREEZE_O_METER_API_KEY)
+    fetch( URL )
+      .then( response => response.json( ))
+      .then( result => {
+        const results = result.data[0]
+        setPollen( results )
+      })
+  }
+
+  return (
+  <>
+    <DesktopContainer
+      setZip={ setZip }
+    />
+    <Results
+      pollenData={ pollen }
+    />
+  </>
+
+)}
+
+export default HomepageLayout
